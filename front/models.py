27085fd4cc3e86b4bea50a37c96ec49d027a4c33
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2015-09-18 19:11:56 +0200

    added mongodb

--- front/models.py
+++ front/models.py
@@ -1,3 +1,3 @@
-from django.db import models
+from mongoengine import *
 
 # Create your models here.
--- rolfsenAndSchmidt/settings.py
+++ rolfsenAndSchmidt/settings.py
@@ -10,6 +10,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import mongoengine
+
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -63,10 +66,11 @@ WSGI_APPLICATION = 'rolfsenAndSchmidt.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': '',
+    },
 }
+
+SESSION_ENGINE = 'mongoengine.django.sessions' # optional
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
