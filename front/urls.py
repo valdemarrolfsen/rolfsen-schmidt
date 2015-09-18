e4abea92eaf9eee9f435144e766653a976af2a59
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2015-09-18 18:39:53 +0200

    first commit

---
+++ front/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
+from front import views
+
+
+viewpatterns = [
+
+    # --------------- Landing related ----------------
+    url(r'^$', views.landing_view, name='landing'),
+
+    # --------------- RestAPI  ---------------
+    #url(r'rest/myjobs', views.MyJobsView.as_view()),
+    #url(r'rest/room/(?P<jobId>\d+)/(?P<applicantId>\d+)/$', views.ChatRoomView.as_view()),
+
+]
+
+urlpatterns = format_suffix_patterns(viewpatterns)
