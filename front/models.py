f41650953a871c95568b3dd6907b0bd375811495
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2015-09-18 20:15:04 +0200

    added example user model

--- front/models.py
+++ front/models.py
@@ -1,3 +1,9 @@
 from mongoengine import *
 
+
 # Create your models here.
+#
+# class User(Document):
+#     email = StringField(required=True)
+#     first_name = StringField(max_length=50)
+#     last_name = StringField(max_length=50)
