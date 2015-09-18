687b1941239e0cb1e5d12863de5a655bdb5f4b73
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2015-09-18 21:15:13 +0200

    Started on layout

--- staticfiles/js/angular/controllers/homeController.js
+++ staticfiles/js/angular/controllers/homeController.js
@@ -3,7 +3,24 @@
  */
 'use strict';
 
-main.controller('homeController', ['$scope', function homeController($scope) {
+main.controller('homeController', ['$scope', '$timeout', function homeController($scope, $timeout) {
 
     $scope.text = 'Hello world';
+
+    $scope.show = {};
+    $scope.show.introHeader = false;
+    $scope.show.introText = false;
+
+
+    $timeout(function() {
+    	$scope.show.introHeader = true;
+    }, 500)
+
+    $timeout(function() {
+    	$scope.show.introText = true;
+    }, 1500)
+
+
+
+
 }]);
