de450f2d729fd753c3275555be7ee0ed2422a2d0
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2016-02-15 21:13:44 +0100

    Added smoother scroll

--- staticfiles/js/angular/directives/navScroll.js
+++ staticfiles/js/angular/directives/navScroll.js
@@ -4,10 +4,12 @@
 main.directive("navScroll", function ($window) {
     return function(scope, element, attrs) {
         angular.element($window).bind("scroll", function() {
-            if (this.pageYOffset >= 310) {
-                element.addClass('with-background');
+            if (this.pageYOffset >= 210) {
+                element.css('background', 'rgba(34, 34, 34,' + (this.pageYOffset-210)/100 + ')');
+            } else if (this.pageYOffset >= 310) {
+                element.css('background', 'rgba(34, 34, 34, 1)');
             } else {
-                element.removeClass('with-background');
+                element.css('background', 'rgba(34, 34, 34, 0)');
             }
         });
     };
