f52e3a2ffa9e69bf94215058bd9f20af1d1b7815
Ruben Schmidt Mällberg <rubschmi@gmail.com>
2016-03-06 19:16:47 +0100

    testing facebook stuff

--- staticfiles/js/angular/controllers/blogPostController.js
+++ staticfiles/js/angular/controllers/blogPostController.js
@@ -1,11 +1,22 @@
 /**
  * Created by rubenschmidt on 05.03.2016.
  */
-main.controller('blogPostController', ['$scope', '$routeParams', 'ArticleFactory','article', function($scope, $routeParams, ArticleFactory, article) {
+main.controller('blogPostController', ['$scope', '$routeParams', 'ArticleFactory', 'article', function ($scope, $routeParams, ArticleFactory, article) {
 
     // Get the article data from the resolved variable
     $scope.article = article.data;
 
     //Set title when route loads
-    window.document.title = $scope.article.title +' | Korde';
+    window.document.title = $scope.article.title + ' | Korde';
+
+    $scope.share = function () {
+        FB.ui(
+            {
+                method: 'feed',
+                name: $scope.article.title,
+                link: 'http://www.korde.no/blog/' + $scope.article.slug,
+                description: $scope.article.body_text,
+                message: ''
+            });
+    }
 }]);
