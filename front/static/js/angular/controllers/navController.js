e2250392fd1341839c5deb2e79569a3169e4fd41
Valdemar Rolfsen <valdemar.rolfsen@gmail.com>
2016-06-24 14:08:14 +0200

    Small fixes

--- front/static/js/angular/controllers/navController.js
+++ front/static/js/angular/controllers/navController.js
@@ -1,8 +1,14 @@
 
 main.controller('navController', ['$scope', '$location', function navController($scope, $location) {
 
+    $scope.showNav = false;
+
     $scope.checkSelected = function(path) {
         return $location.path() == path;
+    }
+
+    $scope.toggleNav = function() {
+        $scope.showNav = !$scope.showNav;
     }
 
 }]);
